c0c68d1fc520b9985218c7259a84d34edc04c080
Den Jansen Flores <den.flores@awsys-i.com>
2025-11-03 01:20:22 +0800

    Added another project

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import Watch from '../assets/projects/luxurywatches.png'
 import Todo from '../assets/projects/Todo.png'
 import Wedding from '../assets/projects/Wedding.png'
+import Employee from '../assets/projects/Employee.png'
 
 export const data=[
     {
@@ -23,5 +24,12 @@ export const data=[
         image:Wedding,
         github:"https://github.com/TsuTsu03/JeizKen",
         live:"https://jeizwantsthedy.vercel.app/",
-    }
+    },
+        {
+        id:3,
+        name:"MULTI-TENANT TICKETING & EMPLOYEE TRACKING SAAS",
+        image:Employee,
+        github:"https://github.com/TsuTsu03/employee-ticketing-system",
+        live:"https://employee-system-sooty.vercel.app/auth/login",
+    },
 ]
